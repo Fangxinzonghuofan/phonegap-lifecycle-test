201f301c97b3e7db390379d6eb450d74f89b0a34
Fangxinzonghuofan <ZHAZ2_15@uni.worc.ac.uk>
2017-11-09 14:16:31 +0000

    Text

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,15 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+$(document).on("pagecreate","#pageone",function(){
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
 
-document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
+
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
 }
 
-
-// device APIs are available
-//
-    function onDeviceReady() {
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
-
-
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
-
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
+}
